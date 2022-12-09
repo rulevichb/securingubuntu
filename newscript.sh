064b91017c69dfbdb926d821a7c83faaf7c41ee6
rulevichb <115268920+rulevichb@users.noreply.github.com>
2022-12-09 14:26:55 -0500

    Update newscript.sh

--- newscript.sh
+++ newscript.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
+# Defining PrintTime Function
 startTime=$(date +"%s")
 printTime()
 {
@@ -23,6 +24,7 @@ printTime()
 	fi
 }
 
+# Checks for Sudo Permission
 echo "Are you running this script as sudo?"
 select yn in "Yes" "No"; do
 	case $yn in
@@ -31,18 +33,20 @@ select yn in "Yes" "No"; do
 	esac
 done
 
-echo "Creating backup folder and script log"
-
+# Creates Backup and Script Log
+echo "Creating backup folder and script log on the Desktop"
 mkdir -p ~/Desktop/backups
 chmod 777 ~/Desktop/backups
-
 touch ~/Desktop/Script.log
 echo > ~/Desktop/Script.log
 chmod 777 ~/Desktop/Script.log
 
+# Backs up group and passwd Files
+echo "Backing up /etc/group and /etc/passwd"
 cp /etc/group ~/Desktop/backups/
 cp /etc/passwd ~/Desktop/backups/
 
+# Checking for Pre-Configured Update Policies
 echo "Have you configured update policies?"
 select yn in "Yes" "No"; do
 	case $yn in
@@ -51,8 +55,15 @@ select yn in "Yes" "No"; do
 	esac
 done
 
-apt-get update && apt-get upgrade -y
-
+# Running Updates
+echo "Running updates"
+echo "Run apt-get update && apt-get upgrade -y?"
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes )  apt-get update && apt-get upgrade -y; break;;
+		No ) break;;
+	esac
+done
 echo "Run apt-get dist-upgrade?"
 select yn in "Yes" "No"; do
 	case $yn in
@@ -61,17 +72,14 @@ select yn in "Yes" "No"; do
 	esac
 done
 
-apt-get install gufw -y && ufw enable 
+# Installing and Enabling Firewall
+echo "Installing and enabling firewall"
+apt-get install gufw -y && ufw enable
 
-apt-get install clamav -y && cd / && freshclam && clamscan
-
-echo "User Managment Stuff"
-
-echo Type all user account names, with a space in between
+# Checking for Correct User Group Settings
+echo "User Group Settings : Type all user account names, with a space in between"
 read -a users
-
 usersLength=${#users[@]}	
-
 for (( i=0;i<$usersLength;i++))
 do
 	echo ${users[${i}]}
@@ -80,7 +88,7 @@ do
 	if [ $yn1 == yes ]
 	then
 		userdel -r ${users[${i}]}
-		printTime "${users[${i}]} has been deleted."
+		printTime "${users[${i}]} has been deleted"
 	else	
 		echo Make ${users[${i}]} administrator? yes or no
 		read yn2								
@@ -90,30 +98,27 @@ do
 			gpasswd -a ${users[${i}]} adm
 			gpasswd -a ${users[${i}]} lpadmin
 			gpasswd -a ${users[${i}]} sambashare
-			printTime "${users[${i}]} has been made an administrator."
+			printTime "${users[${i}]} has been made an administrator"
 		else
 			gpasswd -d ${users[${i}]} sudo
 			gpasswd -d ${users[${i}]} adm
 			gpasswd -d ${users[${i}]} lpadmin
 			gpasswd -d ${users[${i}]} sambashare
 			gpasswd -d ${users[${i}]} root
-			printTime "${users[${i}]} has been made a standard user."
+			printTime "${users[${i}]} has been made a standard user"
 		fi
 	fi
 done
 
-sleep 5
-
-echo Type user account names of users you want to add, with a space in between
+# Adding Users and Properly Assigning Groups
+echo "Adding Users : Type user account names of users you want to add, with a space in between"
 read -a usersNew
-
 usersNewLength=${#usersNew[@]}	
-
 for (( i=0;i<$usersNewLength;i++))
 do
 	echo ${usersNew[${i}]}
 	adduser ${usersNew[${i}]}
-	printTime "A user account for ${usersNew[${i}]} has been created."
+	printTime "A user account for ${usersNew[${i}]} has been created"
 	echo Make ${usersNew[${i}]} administrator? yes or no
 	read ynNew								
 	if [ $ynNew == yes ]
@@ -122,37 +127,34 @@ do
 		gpasswd -a ${usersNew[${i}]} adm
 		gpasswd -a ${usersNew[${i}]} lpadmin
 		gpasswd -a ${usersNew[${i}]} sambashare
-		printTime "${usersNew[${i}]} has been made an administrator."
+		printTime "${usersNew[${i}]} has been made an administrator"
 	else
-		printTime "${usersNew[${i}]} has been made a standard user."
+		printTime "${usersNew[${i}]} has been made a standard user"
 	fi
 done
 
+# Configuring /etc/shadow Permissions
+echo "Setting Read/Write permissions on /etc/shadow"
 chmod 604 /etc/shadow
-printTime "Read/Write permissions on shadow have been set."
 
-chmod 640 .bash_history
-printTime "Bash history file permissions set."
+# Configuring .bash_history Permissions
+echo "Setting file permissions set on /.bash_history"
+chmod 640 ~/.bash_history
 
+# Configuring User Passwords
 echo "Changing User Passwords"
-
 sleep 5
+ls /home > /users.txt 
+awk '{print $0, ":S3cureP@ssw0rd123!"}' /users.txt > /userspasswds.txt 
+sed 's/[[:blank:]]//g' /userspasswds.txt > /userspasswds2.txt 
+chpasswd < /userspasswds2.txt 
 
-ls /home > users.txt 
-
-awk '{print $0, ":S3cureP@ssw0rd123!"}' users.txt > userspasswds.txt 
-
-sed 's/[[:blank:]]//g' userspasswds.txt > userspasswds2.txt 
-
-chpasswd < userspasswds2.txt 
-
+# Locking Root Account
+echo "Root account is being locked. Use 'usermod -U root' to unlock it"
 usermod -L root
-echo "Root account has been locked. Use 'usermod -U root' to unlock it."
 
-sleep 3
-
+# Inputting all Audio Files into root/Desktop
 echo "Inputing Music File Names into Script.log"
-
 find / -name "*.midi" -type f >> ~/Desktop/Script.log
 find / -name "*.mid" -type f >> ~/Desktop/Script.log
 find / -name "*.mod" -type f >> ~/Desktop/Script.log
@@ -170,6 +172,8 @@ find / -name "*.sid" -type f >> ~/Desktop/Script.log
 find / -name "*.flac" -type f >> ~/Desktop/Script.log
 find / -name "*.ogg" -type f >> ~/Desktop/Script.log
 
+# Inputting all Video Files into root/Desktop
+echo "Inputing Video File Names into Script.log"
 find / -name "*.mpeg" -type f >> ~/Desktop/Script.log
 find / -name "*.mpg" -type f >> ~/Desktop/Script.log
 find / -name "*.mpe" -type f >> ~/Desktop/Script.log
@@ -214,6 +218,8 @@ find / -name "*.swf" -type f >> ~/Desktop/Script.log
 find / -name "*.flv" -type f >> ~/Desktop/Script.log
 find / -name "*.m4v" -type f >> ~/Desktop/Script.log
 
+# Inputting all Image Files into root/Desktop
+echo "Inputing Image File Names into Script.log"
 find / -name "*.tiff" -type f >> ~/Desktop/Script.log
 find / -name "*.tif" -type f >> ~/Desktop/Script.log
 find / -name "*.rs" -type f >> ~/Desktop/Script.log
@@ -234,8 +240,7 @@ find / -name "*.ico" -type f >> ~/Desktop/Script.log
 find / -name "*.svg" -type f >> ~/Desktop/Script.log
 find / -name "*.svgz" -type f >> ~/Desktop/Script.log
 
-sleep 5
-
+# Purging Malicious Software
 echo "Purge Malicious Software?"
 select yn in "Yes" "No"; do
 	case $yn in
@@ -246,10 +251,7 @@ done
 
 echo "Finished purges"
 
-sleep 5
-
-echo "Service Managment"
-
+# Asking if Services are Required
 echo Does this machine need Samba?
 read sambaYN
 echo Does this machine need FTP?
@@ -262,13 +264,15 @@ echo Does this machine need Mail?
 read mailYN
 echo Does this machine need Printing?
 read printYN
-echo Does this machine need MySQL?
-read dbYN
+#echo Does this machine need MySQL?
+#read dbYN
 echo Will this machine be a Web Server?
 read httpYN
 echo Does this machine need DNS?
 read dnsYN
 
+# Configuring Samba
+echo "Samba Configuration"
 if [ $sambaYN == no ]
 then
 	ufw deny netbios-ns
@@ -294,21 +298,22 @@ then
 		sed -i 's/####### Authentication #######/####### Authentication #######\nsecurity = user/g' /etc/samba/smb.conf
 	fi
 	sed -i 's/usershare allow guests = no/usershare allow guests = yes/g' /etc/samba/smb.conf
-	
-	echo Type all user account names, with a space in between
+	echo "Type all user account names, with a space in between"
 	read -a usersSMB
 	usersSMBLength=${#usersSMB[@]}	
 	for (( i=0;i<$usersSMBLength;i++))
 	do
 		echo -e 'Moodle!22\nMoodle!22' | smbpasswd -a ${usersSMB[${i}]}
-		printTime "${usersSMB[${i}]} has been given the password 'Moodle!22' for Samba."
+		printTime "${usersSMB[${i}]} has been given the password 'Moodle!22' for Samba"
 	done
 	printTime "netbios-ns, netbios-dgm, netbios-ssn, and microsoft-ds ports have been denied. Samba config file has been configured."
 else
-	echo Response not recognized.
+	echo "Response not recognized"
 fi
-printTime "Samba is complete."
+printTime "Samba is complete"
 
+# Configuring FTP
+echo "FTP Configuration"
 if [ $ftpYN == no ]
 then
 	ufw deny ftp 
@@ -326,6 +331,8 @@ then
 	ufw allow saft 
 	ufw allow ftps-data 
 	ufw allow ftps
+	apt-get install vsftpd -y
+	apt-get install pure-ftpd -y
 	cp /etc/vsftpd/vsftpd.conf ~/Desktop/backups/
 	cp /etc/vsftpd.conf ~/Desktop/backups/
 	gedit /etc/vsftpd/vsftpd.conf&gedit /etc/vsftpd.conf
@@ -333,10 +340,12 @@ then
 	service pure-ftpd
 	printTime "ftp, sftp, saft, ftps-data, and ftps ports have been allowed on the firewall. vsFTPd service has been restarted."
 else
-	echo Response not recognized.
+	echo "Response not recognized"
 fi
-printTime "FTP is complete."
+printTime "FTP is complete"
 
+# Configuring SSH
+echo "SSH Configuration"
 if [ $sshYN == no ]
 then
 	ufw deny ssh
@@ -347,7 +356,7 @@ then
 	apt-get install openssh-server -y -qq
 	ufw allow ssh
 	cp /etc/ssh/sshd_config ~/Desktop/backups/	
-	echo Type all user account names, with a space in between
+	echo "Type all user account names, with a space in between"
 	read usersSSH
 	service ssh restart
 	mkdir ~/.ssh
@@ -355,10 +364,12 @@ then
 	ssh-keygen -t rsa
 	printTime "SSH port has been allowed on the firewall. SSH config file has been configured. SSH RSA 2048 keys have been created."
 else
-	echo Response not recognized.
+	echo "Response not recognized"
 fi
-printTime "SSH is complete."
+printTime "SSH is complete"
 
+# Configuring Telnet
+echo "Telnet Configuration"
 if [ $telnetYN == no ]
 then
 	ufw deny telnet 
@@ -374,12 +385,14 @@ then
 	ufw allow telnet 
 	ufw allow rtelnet 
 	ufw allow telnets
-	printTime "Telnet port has been allowed on the firewall."
+	printTime "Telnet port has been allowed on the firewall"
 else
-	echo Response not recognized.
+	echo "Response not recognized"
 fi
-printTime "Telnet is complete."
+printTime "Telnet is complete"
 
+# Configuring Mail Services
+echo "Mail Service Configuration"
 if [ $mailYN == no ]
 then
 	ufw deny smtp 
@@ -397,70 +410,76 @@ then
 	ufw allow imap2 
 	ufw allow imaps 
 	ufw allow pop3s
-	printTime "smtp, pop2, pop3, imap2, imaps, and pop3s ports have been allowed on the firewall."
+	printTime "smtp, pop2, pop3, imap2, imaps, and pop3s ports have been allowed on the firewall"
 else
-	echo Response not recognized.
+	echo "Response not recognized"
 fi
-printTime "Mail is complete."
+printTime "Mail is complete"
 
+# Configuring Print Services
+echo "Print Service Configuration"
 if [ $printYN == no ]
 then
 	ufw deny ipp 
 	ufw deny printer 
 	ufw deny cups
-	printTime "ipp, printer, and cups ports have been denied on the firewall."
+	printTime "ipp, printer, and cups ports have been denied on the firewall"
 elif [ $printYN == yes ]
 then
 	ufw allow ipp 
 	ufw allow printer 
 	ufw allow cups
-	printTime "ipp, printer, and cups ports have been allowed on the firewall."
+	printTime "ipp, printer, and cups ports have been allowed on the firewall"
 else
-	echo Response not recognized.
+	echo "Response not recognized"
 fi
-printTime "Printing is complete."
+printTime "Printing is complete"
 
-if [ $dbYN == no ]
-then
-	ufw deny ms-sql-s 
-	ufw deny ms-sql-m 
-	ufw deny mysql 
-	ufw deny mysql-proxy
-	apt-get purge mysql -y -qq
-	apt-get purge mysql-client-core-5.5 -y -qq
-	apt-get purge mysql-client-core-5.6 -y -qq
-	apt-get purge mysql-common-5.5 -y -qq
-	apt-get purge mysql-common-5.6 -y -qq
-	apt-get purge mysql-server -y -qq
-	apt-get purge mysql-server-5.5 -y -qq
-	apt-get purge mysql-server-5.6 -y -qq
-	apt-get purge mysql-client-5.5 -y -qq
-	apt-get purge mysql-client-5.6 -y -qq
-	apt-get purge mysql-server-core-5.6 -y -qq
-	printTime "ms-sql-s, ms-sql-m, mysql, and mysql-proxy ports have been denied on the firewall. MySQL has been removed."
-elif [ $dbYN == yes ]
-then
-	ufw allow ms-sql-s 
-	ufw allow ms-sql-m 
-	ufw allow mysql 
-	ufw allow mysql-proxy
-	apt-get install mysql-server-5.6 -y -qq
-	cp /etc/my.cnf ~/Desktop/backups/
-	cp /etc/mysql/my.cnf ~/Desktop/backups/
-	cp /usr/etc/my.cnf ~/Desktop/backups/
-	cp ~/.my.cnf ~/Desktop/backups/
-	if grep -q "bind-address" "/etc/mysql/my.cnf"
-	then
-		sed -i "s/bind-address\t\t=.*/bind-address\t\t= 127.0.0.1/g" /etc/mysql/my.cnf
-	fi
-	gedit /etc/my.cnf&gedit /etc/mysql/my.cnf&gedit /usr/etc/my.cnf&gedit ~/.my.cnf
-	service mysql restart
-	printTime "ms-sql-s, ms-sql-m, mysql, and mysql-proxy ports have been allowed on the firewall. MySQL has been installed. MySQL config file has been secured. MySQL service has been restarted."
-else
-	echo Response not recognized.
-fi
-printTime "MySQL is complete."
+# Configuring MySQL Database
+#echo "MySQL Database Configuration"
+#if [ $dbYN == no ]
+#then
+#	ufw deny ms-sql-s 
+#	ufw deny ms-sql-m 
+#	ufw deny mysql 
+#	ufw deny mysql-proxy
+#	apt-get purge mysql -y -qq
+#	apt-get purge mysql-client-core-5.5 -y -qq
+#	apt-get purge mysql-client-core-5.6 -y -qq
+#	apt-get purge mysql-common-5.5 -y -qq
+#	apt-get purge mysql-common-5.6 -y -qq
+#	apt-get purge mysql-server -y -qq
+#	apt-get purge mysql-server-5.5 -y -qq
+#	apt-get purge mysql-server-5.6 -y -qq
+#	apt-get purge mysql-client-5.5 -y -qq
+#	apt-get purge mysql-client-5.6 -y -qq
+#	apt-get purge mysql-server-core-5.6 -y -qq
+#	printTime "ms-sql-s, ms-sql-m, mysql, and mysql-proxy ports have been denied on the firewall. MySQL has been removed."
+#elif [ $dbYN == yes ]
+#then
+#	ufw allow ms-sql-s 
+#	ufw allow ms-sql-m 
+#	ufw allow mysql 
+#	ufw allow mysql-proxy
+#	apt-get install mysql-server-5.6 -y -qq
+#	cp /etc/my.cnf ~/Desktop/backups/
+#	cp /etc/mysql/my.cnf ~/Desktop/backups/
+#	cp /usr/etc/my.cnf ~/Desktop/backups/
+#	cp ~/.my.cnf ~/Desktop/backups/
+#	if grep -q "bind-address" "/etc/mysql/my.cnf"
+#	then
+#		sed -i "s/bind-address\t\t=.*/bind-address\t\t= 127.0.0.1/g" /etc/mysql/my.cnf
+#	fi
+#	gedit /etc/my.cnf&gedit /etc/mysql/my.cnf&gedit /usr/etc/my.cnf&gedit ~/.my.cnf
+#	service mysql restart
+#	printTime "ms-sql-s, ms-sql-m, mysql, and mysql-proxy ports have been allowed on the firewall. MySQL has been installed. MySQL config file has been secured. MySQL service has been restarted."
+#else
+#	echo "Response not recognized"
+#fi
+#printTime "MySQL is complete"
 
+# Configuring HTTP
+echo "HTTP Configuration"
 if [ $httpYN == no ]
 then
 	ufw deny http
@@ -482,10 +501,12 @@ then
 
 	printTime "http and https ports have been allowed on the firewall. Apache2 config file has been configured. Only root can now access the Apache2 folder."
 else
-	echo Response not recognized.
+	echo "Response not recognized"
 fi
-printTime "Web Server is complete."
+printTime "Web Server is complete"
 
+# Configuring DNS
+echo "DNS Configuration"
 if [ $dnsYN == no ]
 then
 	ufw deny domain
@@ -494,13 +515,30 @@ then
 elif [ $dnsYN == yes ]
 then
 	ufw allow domain
-	printTime "domain port has been allowed on the firewall."
+	printTime "domain port has been allowed on the firewall"
 else
-	echo Response not recognized.
+	echo "Response not recognized"
 fi
-printTime "DNS is complete."
+printTime "DNS is complete"
 
-echo "Delete files created for passwords?"
+# Backing up Crontab, Removing Startup Tasks, and Only Allowing Root
+echo "Backing up Crontab, Removing Startup Tasks, and Only Allowing Root in Cron"
+crontab -l > ~/Desktop/crontab-old
+crontab -r
+cd /etc/
+/bin/rm -f cron.deny at.deny
+echo root >cron.allow
+echo root >at.allow
+/bin/chown root:root cron.allow at.allow
+/bin/chmod 400 cron.allow at.allow
+cd ..
+
+# Fixing Shellshock Bash Vulnerability
+echo "Running Shellshock Bash Vulnerability Test"
+env i='() { :;}; echo Your system is Bash vulnerable' bash -c "echo Bash vulnerability test"
+
+# Deleting Temp Password Files
+echo "Delete temp files created for passwords?"
 select yn in "Yes" "No"; do
 	case $yn in
 		Yes )  rm /users.txt; rm /userspasswds.txt; rm /userspasswds2.txt; break;;
@@ -508,10 +546,9 @@ select yn in "Yes" "No"; do
 	esac
 done
 
+# Autoremoving Unused Packages
 echo "Removing unused packages"
-
-sleep 3
-
+sleep 5
 apt-get autoremove -y
 apt-get autoclean -y
 apt-get clean -y
